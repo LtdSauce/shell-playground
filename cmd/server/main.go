e3680a56eaad64d4eb2c3072cf03b67937738142
Lucas Detje <lucas.detje@be-storaged.com>
2024-03-18 21:59:16 +0100

    Add a basic server that serves the html page for devel

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,9 +6,11 @@ import (
 )
 
 func main() {
+	fmt.Println("Starting Server")
 	err := http.ListenAndServe(":9090", http.FileServer(http.Dir("../../assets")))
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
 	}
+	fmt.Println("Started or stopped?")
 }
