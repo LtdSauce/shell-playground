24f9c040378db00ce75c8a5b28029ec895d1c615
Lucas Detje <lucas.detje@be-storaged.com>
2024-03-23 00:10:35 +0100

    Add 2 spaces when tab is hit in the input text area
    Before this, hitting tab would navigate to the next html element...
    which is counter intuitive when "editing" a shell script.

--- main.go
+++ main.go
@@ -10,6 +10,8 @@ import (
 	"github.com/maxence-charriere/go-app/v9/pkg/app"
 )
 
+const TabSize = 2
+
 type playground struct {
 	app.Compo
 
@@ -30,7 +32,7 @@ func formatShellScript(input string) (string, error) {
 }
 
 func (p *playground) onFormat(ctx app.Context, e app.Event) {
-	fmt.Printf("Formatting input:\n%s", p.ShellInput)
+	fmt.Printf("Formatting input:\n%s\n", p.ShellInput)
 	formatted, err := formatShellScript(p.ShellInput)
 	if err != nil {
 		p.FormattedOutput = "Whoopsi something went wrong"
@@ -39,9 +41,29 @@ func (p *playground) onFormat(ctx app.Context, e app.Event) {
 	p.FormattedOutput = formatted
 }
 
+// This is meant to insert "tabs" instead of tabbing out of the textarea
+func (p *playground) onKeyDown(ctx app.Context, e app.Event) {
+	if e.Get("key").String() == "Tab" {
+		e.PreventDefault()
+		// Preserve current caret position
+		// Not sure why Start and End seperatly as i expect them to be the same :shrug:
+		start := ctx.JSSrc().Get("selectionStart").Int()
+		end := ctx.JSSrc().Get("selectionEnd").Int()
+
+		// If we received a tab we need to insert the specified amount of spaces at the right position.
+		value := ctx.JSSrc().Get("value").String()
+		value = value[:start] + strings.Repeat(" ", TabSize) + value[end:]
+		ctx.JSSrc().Set("value", value)
+
+		// update caret to reflect inserted spaces for the tab
+		ctx.JSSrc().Set("selectionStart", start+TabSize)
+		ctx.JSSrc().Set("selectionEnd", end+TabSize)
+	}
+}
+
 func (p *playground) Render() app.UI {
 	return app.Div().Body(
-		app.Textarea().ID("shellinput").Name("shellinput").Text(p.ShellInput).Cols(80).Rows(20).OnInput(p.ValueTo(&p.ShellInput)),
+		app.Textarea().ID("shellinput").Name("shellinput").Text(p.ShellInput).Cols(80).Rows(20).OnInput(p.ValueTo(&p.ShellInput)).OnKeyDown(p.onKeyDown),
 		app.Input().ID("run-format").Value("run formatter").Type("submit").OnClick(p.onFormat),
 		app.Textarea().ID("formattedoutput").Name("formattedoutput").Text(p.FormattedOutput).Cols(80).Rows(20),
 	)
